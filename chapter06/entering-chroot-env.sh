62d42301b65362e5eb147d71e73fd777fa995654
Tobias Dreyer <tewdreyer@gmail.com>
2012-05-30 23:13:25 +0200

    basic system: add script to enter the chroot environment

---
+++ chapter06/entering-chroot-env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+: ${LFS:=/mnt/lfs}
+
+chroot "${LFS}" /tools/bin/env -i \
+    HOME=/root TERM="${TERM}" PS1='\u:\w\$ ' \
+    PATH=/bin:/usr/bin:/sbin:/usr/sbin:/tools/bin \
+    /tools/bin/bash --login +h
