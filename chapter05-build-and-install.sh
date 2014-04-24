cf91b182a90655f1e3a66b2b81a85bdc3b18103e
Tobias Dreyer <tewdreyer@gmail.com>
2014-04-24 11:42:23 +0200

    chapter 5.7: update glibc to 2.19 for LFS 7.5

--- chapter05-build-and-install.sh
+++ chapter05-build-and-install.sh
@@ -8,7 +8,7 @@ for bee in \
     tc-binutils_pass1-2.24-0.bee \
     tc-gcc_pass1-4.8.2-0.bee \
     tc-linux-api-headers-3.13.3-0.bee \
-    tc-glibc-2.14.1-0.bee \
+    tc-glibc-2.19-0.bee \
     tc-adjust-toolchain-0-0.bee \
     tc-binutils_pass2-2.22-0.bee \
     tc-gcc_pass2-4.6.2-0.bee \
