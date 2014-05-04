d0fe3ab4e226fd666d6e266f0e57d6cdc95dcd6e
Tobias Dreyer <tewdreyer@gmail.com>
2014-05-04 16:46:42 +0200

    chapter 6.9: add glibc 2.19

--- chapter06-build-and-install.sh
+++ chapter06-build-and-install.sh
@@ -7,7 +7,8 @@ source _load-config.lib
 for bee in \
     fhs-1-0.bee \
     linux-api-headers-3.13.3-0.bee \
-    man-pages-3.59-0.bee
+    man-pages-3.59-0.bee \
+    glibc-2.19-0.bee
 do
     beesh "chapter06/${bee}" --no-archive-build "${@}" --cleanup
 
