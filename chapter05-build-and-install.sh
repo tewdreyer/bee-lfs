7d33eeb050cb018c90c10ed1b07702bce8f305ab
Tobias Dreyer <tewdreyer@gmail.com>
2014-05-04 15:03:42 +0200

    bee-lfs toolchain: wget dependencies - add gmp 5.1.3

--- chapter05-build-and-install.sh
+++ chapter05-build-and-install.sh
@@ -35,7 +35,8 @@ for bee in \
     tc-tar-1.27.1-0.bee \
     tc-texinfo-5.2-0.bee \
     tc-util-linux-2.24.1-0.bee \
-    tc-xz-5.0.5-0.bee ; do
+    tc-xz-5.0.5-0.bee \
+    tc-gmp-5.1.3-0.bee ; do
   beesh "chapter05/${bee}" --install --no-archive-build "${@}" --cleanup
 done
 
