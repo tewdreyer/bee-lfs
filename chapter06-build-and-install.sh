16ccf012a9ee66d9832e73245f6c31457491245a
Tobias Dreyer <tewdreyer@gmail.com>
2014-05-04 17:19:49 +0200

    chapter 6.11: add zlib 1.2.8

--- chapter06-build-and-install.sh
+++ chapter06-build-and-install.sh
@@ -8,7 +8,8 @@ for bee in \
     fhs-1-0.bee \
     linux-api-headers-3.13.3-0.bee \
     man-pages-3.59-0.bee \
-    glibc-2.19-0.bee
+    glibc-2.19-0.bee \
+    zlib-1.2.8-0.bee
 do
     beesh "chapter06/${bee}" --no-archive-build "${@}" --cleanup
 
