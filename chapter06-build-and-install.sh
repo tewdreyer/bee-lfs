b7e4916dfe3edcfe7ef69fc2970c39774433de8f
Tobias Dreyer <tewdreyer@gmail.com>
2014-05-04 18:47:30 +0200

    chapter 6.12: add file 5.17

--- chapter06-build-and-install.sh
+++ chapter06-build-and-install.sh
@@ -9,7 +9,9 @@ for bee in \
     linux-api-headers-3.13.3-0.bee \
     man-pages-3.59-0.bee \
     glibc-2.19-0.bee \
-    zlib-1.2.8-0.bee
+    zlib-1.2.8-0.bee \
+    file-5.17-0.bee \
+
 do
     beesh "chapter06/${bee}" --no-archive-build "${@}" --cleanup
 
