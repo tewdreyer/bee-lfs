f1f0bee52af0abd49086a945dd70acb103d6adeb
Tobias Dreyer <tewdreyer@gmail.com>
2014-05-05 21:38:13 +0200

    bee-lfs toolchain: gcc dependencies - add zlib 1.2.8

--- chapter05-build-and-install.sh
+++ chapter05-build-and-install.sh
@@ -41,7 +41,8 @@ for bee in \
     tc-gnutls-3.2.13-0.bee \
     tc-wget-1.15-0.bee \
     tc-mpfr-3.1.2-0.bee \
-    tc-mpc-1.0.2-0.bee ; do
+    tc-mpc-1.0.2-0.bee \
+    tc-zlib-1.2.8-0.bee ; do
   beesh "chapter05/${bee}" --install --no-archive-build "${@}" --cleanup
 done
 
