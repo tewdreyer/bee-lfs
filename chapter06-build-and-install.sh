ba23a479bc76af24acf1574b410c98e7cadd6e8f
Tobias Dreyer <tewdreyer@gmail.com>
2014-05-06 12:02:04 +0200

    chapter 6.22: add shadow 4.1.5.1

--- chapter06-build-and-install.sh
+++ chapter06-build-and-install.sh
@@ -20,6 +20,7 @@ for bee in \
     bzip2-1.0.6-0.bee \
     pkg-config-0.28-0.bee \
     ncurses-5.9-0.bee \
+    shadow-4.1.5.1-0.bee \
 
 do
     beesh "chapter06/${bee}" --no-archive-build "${@}" --cleanup
