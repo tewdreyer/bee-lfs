fcf0572f125c8af1d4c6fd6dcbd87771ed05b1bd
Tobias Dreyer <tewdreyer@gmail.com>
2014-05-14 23:58:12 +0200

    chapter 6.47: add grub 2.0

--- chapter06-build-and-install.sh
+++ chapter06-build-and-install.sh
@@ -45,6 +45,7 @@ for bee in \
     gettext-0.18.3.2-0.bee \
     groff-1.22.2-0.bee \
     xz-5.0.5-0.bee \
+    grub-2.00-0.bee \
 
 do
     beesh "chapter06/${bee}" --no-archive-build "${@}" --cleanup
